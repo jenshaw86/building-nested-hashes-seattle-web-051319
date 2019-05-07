f142080170f83964c9a9ce23fcce1f084b3262fe
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-07 05:54:18 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,8 +7,12 @@ def second_challenge
       :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }
+      :patriach => {}, 
+      :matriach => {}, 
+      :heroine => {},
+      :heroine_friends => []
+      }, 
+  }
 
   
 end
